0a62e006eff50c972f1519b95a6cb413e98bec70
seung.yeop <milnil0228@gmail.com>
2022-11-28 11:40:13 +0900

    docs:메인 수정:예시 리스트 제거

--- src/Main.java
+++ src/Main.java
@@ -18,21 +18,6 @@ public class Main {
         Room room = new Room();
         Member member = new Member();
 
-        hotel.addNumberList(new Member(1, "홍승엽", "010-1111-3113", 100000));
-        hotel.addNumberList(new Member(2, "이승윤", "010-2222-3113", 200000));
-        hotel.addNumberList(new Member(3, "김민선", "010-3333-3113", 300000));
-        hotel.addNumberList(new Member(4, "노예진", "010-4444-3113", 400000));
-        hotel.addNumberList(new Member(5, "한세인", "010-5555-3113", 500000));
-
-            //룸 리스트 추가 예시
-        hotel.addRoomList(new Room(201,1,10000,"2022.12.12"));
-        hotel.addRoomList(new Room(202,2,20000,"2022.12.12"));
-        hotel.addRoomList(new Room(203,3,30000,"2022.12.12"));
-        hotel.addRoomList(new Room(204,4,40000,"2022.12.12"));
-        hotel.addRoomList(new Room(201,1,10000,"2022.12.13"));
-        hotel.addRoomList(new Room(201,1,10000,"2022.12.14"));
-        hotel.addRoomList(new Room(203,2,10000,"2022.12.14"));
-
         System.out.println(hotel.getRoomList().get(1).getReservationDate());
 
         System.out.println(hotel.getMemberList().get(0));
