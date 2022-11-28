3e03d2f0bb1827e83df03a24c9a55a5e1bc9cf5d
seinhan <sein151@naver.com>
2022-11-28 22:47:15 +0900

    [docs] HotelController cancelReservation메소드 수정

--- src/Data/HotelController.java
+++ src/Data/HotelController.java
@@ -272,7 +272,7 @@ public class HotelController {
                 Reservation reservation = reservationsList.get(i);
 
                 int hotelMinusMoney = memberPlusMoney(reservation.getMemberId(), reservation.getRoomNum(), hotel);
-                long hotelTotalMoney = hotel.getTotalMoney();
+                long hotelTotalMoney = hotel.getTotalMoney()-hotelMinusMoney;
 
                 hotel.setTotalMoney(hotelTotalMoney - hotelMinusMoney);
 
